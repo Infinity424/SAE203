d2aa66ab4d725f6db70d37eba0d9f4267de5ed87
Serveur (fichier detail_commit) <jean-baptiste.aubry@etudiant.univ-rennes.fr>
2026-06-05 15:13:51 +0000

    modif plug

--- wordpress/wp-content/plugins/wp-partenaires/wp-partenaires.php
+++ wordpress/wp-content/plugins/wp-partenaires/wp-partenaires.php
@@ -1,63 +1,41 @@
 <?php
 /*
-Plugin Name: Affichage Partenaires
-Description: Un plugin de pour voir les Partenaires 
+Plugin Name: Partenaires
+Description: Un plugin de pour gerer les partenaires
 Version: 1.0
 Author: Techlok
 */
 
-add_shortcode( 'Affiche_Partenaire', 'wp_temoignages_display_shortcode' );
+shortcode_atts('partenaires', 'affiche_partenaire');
+function affiche_partenaire(){
+    $json_url = 'http://172.18.203.208/intranet/data/annuaire_partenaires.json';
+    $response = wp_remote_get($json_url);
 
-function wp_temoignages_display_shortcode() {
-    wp_enqueue_style(
-        'bootstrap-5',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
-        array(),
-        '5.3.3'
-    );
-
-    wp_enqueue_script(
-        'bootstrap-5-js',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
-        array(),
-        '5.3.3',
-        true
-    );
-    wp_add_inline_style('bootstrap-5', '
-    body {
-        background-color: rgb(11, 21, 38) !important;
-        color : rgb(255, 255, 255) !important;
+    if(is_wp_error($response)){
+        return 'Erreur lors du chargement de la page !';
     }
-    ');
-    //j'ai pas réusi a fair en sorte que bootstrap ne change pas le texte et le background ducoup j'ai forcer les couleur qui on été utilisé (pas la meilleur solution mais j'ai pas réusi a trouver mieux)
-    $fichier  = '../intranet/data/annuaire_commpar.json';
-    $partenaires = json_decode(file_get_contents($fichier), true);
+
+    $contenu = file_get_contents($json_url);
+    $partenaires = json_decode($contenu , true);
     ob_start(); // Démarre la temporisation
-    ?>
-    <h2 class="text-center mb-4" style="color:#1D9E75;">Liste partenaires</h2>
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mb-4">
-        <?php
-        foreach ($partenaires as $partenaire):
-        ?>
-            <div class="col">
-                <div class="card h-100 shadow-sm">
-                    <div class="card-header bg-dark text-white d-flex justify-content-between">
-                        <span><?php echo htmlspecialchars(ucfirst($partenaire['nom'])); ?></span>
-                        <img src="<?php echo htmlspecialchars($partenaire['logo']); ?>" alt="Logo" class="img-fluid mb-2" style="max-height:80px;">
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            <strong>Contact :</strong> <?php echo htmlspecialchars($partenaire['contact']); ?><br>
-                            <strong>Secteur :</strong> <?php echo htmlspecialchars($partenaire['secteur']); ?><br>
-                            <strong>Email :</strong> <?php echo htmlspecialchars($partenaire['email']); ?><br>
-                            <strong>Téléphone :</strong> <?php echo htmlspecialchars($partenaire['telephone']); ?><br>
-                            <strong>Précision :</strong> <?php echo htmlspecialchars($partenaire['detail']); ?><br>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
-        </div>
-    <?php return ob_get_clean(); // Retourne le contenu mis en cache
+    $html = '<div class="partenaires-grid">';
+
+    foreach($partenaires as $partenaire){
+        $logo = esc_html($partenaire['logo']);
+        $description = esc_html($partenaire['descritption']);
+
+        $html .= '<div class="partenaire-item">';
+        $html .= '<img src = "' . $logo . '" alt="Logo de ' . $partenaire['nom'] . '" class="partenaire-logo"> ';
+        $html .= '<p>"' . $description .'"</p> ';
+
+        $html .= '</div>';
+        
+
+
+    }
+        
+    return ob_get_clean();
+
 }
+
 ?>
