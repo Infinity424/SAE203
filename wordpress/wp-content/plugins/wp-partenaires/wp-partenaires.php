16bf44278d6b3d902604c5687b115546de24af15
gwendal67 <gwendal67@gmail.com>
2026-06-06 12:13:01 +0200

    correction du shortcode ajout gestion d'erreur, récupération du fichier et lecture du tableau Correction de l'affichage

--- wordpress/wp-content/plugins/wp-partenaires/wp-partenaires.php
+++ wordpress/wp-content/plugins/wp-partenaires/wp-partenaires.php
@@ -6,36 +6,43 @@ Version: 1.0
 Author: Techlok
 */
 
-shortcode_atts('partenaires', 'affiche_partenaire');
+add_shortcode('partenaires', 'affiche_partenaire');
+
 function affiche_partenaire(){
-    $json_url = '../intranet/data/annuaire_partenaires.json';
+    $json_url = plugin_dir_path(__FILE__) . '../intranet/data/annuaire_partenaires.json';
     $response = wp_remote_get($json_url);
 
-    if(is_wp_error($response)){
+    if(!file_exists($json_url)){
         return 'Erreur lors du chargement de la page !';
     }
 
     $contenu = file_get_contents($json_url);
     $partenaires = json_decode($contenu , true);
-    ob_start(); // Démarre la temporisation
-    $html = '<div class="partenaires-grid">';
 
-    foreach($partenaires as $partenaire){
+    if(!is_array($partenaires)){
+        return 'Erreur lors de la lecture des partenaires !';
+    }
+    ob_start(); // Démarre la temporisation
+
+    ?>
+
+
+  <div class="partenaires-grid">'
+
+    <?php foreach($partenaires as $partenaire) :
         $logo = esc_html($partenaire['logo']);
         $description = esc_html($partenaire['descritption']);
+        $nom = esc_html($partenaire['nom']);
+        ?>
 
-        $html .= '<div class="partenaire-item">';
-        $html .= '<img src = "' . $logo . '" alt="Logo de ' . $partenaire['nom'] . '" class="partenaire-logo"> ';
-        $html .= '<p>"' . $description .'"</p> ';
+        <div class="partenaire-item">'
+        <img src = " <?php echo $logo; ?>" alt="Logo de <?php echo $nom; ?>" class="partenaire-logo"> ';
+        <p>"<?php echo $description; ?>"</p> 
+        </div>
+    <?php endforeach; ?>
+    </div>
 
-        $html .= '</div>';
-        
-
-
-    }
-        
-    return ob_get_clean();
+    <?php return ob_get_clean();
 
 }
-
 ?>
