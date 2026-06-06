0d323428cba76286d6bdcebd51ebcbdef394c174
gwendal67 <gwendal67@gmail.com>
2026-06-06 12:27:22 +0200

    correction url et guillemets

--- wordpress/wp-content/plugins/wp-partenaires/wp-partenaires.php
+++ wordpress/wp-content/plugins/wp-partenaires/wp-partenaires.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Partenaires
-Description: Un plugin de pour gerer les partenaires
+Description: Un plugin pour gerer les partenaires
 Version: 1.0
 Author: Techlok
 */
@@ -9,40 +9,39 @@ Author: Techlok
 add_shortcode('partenaires', 'affiche_partenaire');
 
 function affiche_partenaire(){
-    $json_url = '../../intranet/data/annuaire_partenaires.json';
-    $response = wp_remote_get($json_url);
+    $json_url = ABSPATH . 'intranet/data/annuaire_partenaires.json';
 
     if(!file_exists($json_url)){
-        return 'Erreur lors du chargement de la page !';
+        return 'Erreur : Le fichier annuaire_partenaires.json est introuvable.';
     }
 
     $contenu = file_get_contents($json_url);
-    $partenaires = json_decode($contenu , true);
+    $partenaires = json_decode($contenu, true);
 
     if(!is_array($partenaires)){
         return 'Erreur lors de la lecture des partenaires !';
     }
-    ob_start(); // Démarre la temporisation
 
+    ob_start(); 
     ?>
 
+    <div class="partenaires-grid">
 
-  <div class="partenaires-grid">'
+        <?php foreach($partenaires as $partenaire) :
+            $logo = esc_url($partenaire['logo']);
+            $description = esc_html($partenaire['descritption']); 
+            $nom = esc_attr($partenaire['nom']);
+            ?>
 
-    <?php foreach($partenaires as $partenaire) :
-        $logo = esc_html($partenaire['logo']);
-        $description = esc_html($partenaire['descritption']);
-        $nom = esc_html($partenaire['nom']);
-        ?>
+            <div class="partenaire-item">
+                <img src="<?php echo $logo; ?>" alt="Logo de <?php echo $nom; ?>" class="partenaire-logo">
+                <p>"<?php echo $description; ?>"</p> 
+            </div>
+        <?php endforeach; ?>
 
-        <div class="partenaire-item">'
-        <img src = " <?php echo $logo; ?>" alt="Logo de <?php echo $nom; ?>" class="partenaire-logo"> ';
-        <p>"<?php echo $description; ?>"</p> 
-        </div>
-    <?php endforeach; ?>
     </div>
 
-    <?php return ob_get_clean();
-
+    <?php 
+    return ob_get_clean();
 }
 ?>
